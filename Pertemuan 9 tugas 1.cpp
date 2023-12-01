dec6f0efac594a1ea51d0fbc714e2ebf9e7eba3a
Henryjhrs <147039010+Henryjhrs@users.noreply.github.com>
2023-12-01 19:55:17 +0700

    Update Pertemuan 9 tugas 1.cpp

--- Pertemuan 9 tugas 1.cpp
+++ Pertemuan 9 tugas 1.cpp
@@ -4,17 +4,14 @@ int main() {
     int A[11] = {12, 17, 10, 5, 15, 25, 11, 7, 25, 16, 19};
     int N, i, ada = 0;
 
-    // Menampilkan isi array
     printf("Isi array:\n");
     for (i = 0; i < 11; i++) {
         printf("%d ", A[i]);
     }
 
-    // Input nilai integer N
     printf("\nMasukkan bilangan integer = ");
     scanf("%d", &N);
 
-    // Mengecek apakah N ada di dalam array
     printf("Hasil: ");
     for (i = 0; i < 11; i++) {
         if (A[i] == N) {
@@ -23,8 +20,7 @@ int main() {
             break;
         }
     }
-
-    // Menampilkan lokasi (index) bilangan yang sama dengan N
+    
     if (ada) {
         for (i = 0; i < 11; i++) {
             if (A[i] == N) {
